b028596b2c68936c2caf8d858149e686c1fa73ae
leogersen <46613350+leogersen@users.noreply.github.com>
2019-02-19 12:10:45 -0300

    Back-end custom Menu

--- public_html/wp-content/themes/Sunset/inc/function-admin.php
+++ public_html/wp-content/themes/Sunset/inc/function-admin.php
@@ -9,11 +9,20 @@ function sunset_add_admin_page() {
   /* Generate Sunset Admin Sub Pages */
   add_submenu_page( "gersen_sunset", "Sunset Theme Options", "Settings", "manage_options", "gersen_sunset", "sunset_theme_create_page" );
   add_submenu_page( "gersen_sunset", "Sunset CSS Options", "Custom CSS", "manage_options", "gersen_sunset_css", "sunset_theme_settings_page");
+
+  /* Activate Custom Settings */
+  add_action( "admin_init", "sunset_custom_settings" ) ;
 }
   add_action( "admin_menu", "sunset_add_admin_page" ) ;
 
+function sunset_custom_settings() {
+  register_setting( "sunset-settings-group", "first_name" ) ;
+}
+
 function sunset_theme_create_page() {
-  echo "<h1>Sunset theme Options</h1>" ;
+
+  require_once( get_template_directory() . "/inc/templates/sunset-admin.php" );
+
 /* Admin page generarion */
 }
 function sunset_theme_settings_page() {
---
+++ public_html/wp-content/themes/Sunset/inc/templates/sunset-admin.php
@@ -0,0 +1,7 @@
+<h1>Sunset Theme Options</h1>
+<h3 class="title">Manage Options</h3>
+<p>Customize Sidebar Options</p>
+
+<form method="post" action="">
+  <?php settings_fields( "sunset-settings-group" ); ?>
+</form>
